c7596c2c6afe8ff994eeb6f1f5c49e04e5bf4d17
veniamin zelenivskyy <zelenivskyy.veniamin@cgm.com>
2021-04-09 12:52:44 +0300

    [master] added fisrt components

---
+++ src/menu/menu-container.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MenuContainer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return (<div id='main'>Menu container</div>);
+    }
+}
+export default MenuContainer;
